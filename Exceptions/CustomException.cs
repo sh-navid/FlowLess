b2857de76d84830acf0fcfb14c0bfe3a32ac5549
sh-navid <shamshirsaz.navid@live.com>
2025-07-17 09:19:50 +0330

    [Feature] Setup exceptions

---
+++ Exceptions/CustomException.cs
@@ -0,0 +1,18 @@
+/// <summary>
+/// A custom exception class that inherits from the base Exception class.
+/// This allows for the creation of specific exception types within the application.
+/// </summary>
+/// <remarks>
+/// This is just an example and can ve removed later
+/// </remarks>
+namespace NoFlowEngine.Exceptions
+{
+    public class CustomException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomException"/> class with a specified error message.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public CustomException(string message) : base(message) { }
+    }
+}
---
+++ Exceptions/GlobalExceptionHandler.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace NoFlowEngine.Exceptions
+{
+    /// <summary>
+    /// Provides extension methods to configure global exception handling for an ASP.NET Core application.
+    /// </summary>
+    public static class GlobalExceptionHandler
+    {
+        /// <summary>
+        /// Configures the exception handler middleware to catch and handle unhandled exceptions.
+        /// It logs the error and returns a standardized error response to the client.
+        /// </summary>
+        /// <param name="app">The <see cref="IApplicationBuilder"/> to add the middleware to.</param>
+        /// <param name="logger">The <see cref="ILogger"/> instance used for logging errors.</param>
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app, ILogger logger)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        logger.LogError($"Something went wrong: {contextFeature.Error}");
+
+                        await context.Response.WriteAsync(new
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = "Internal Server Error. Please contact support."
+                        }.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
