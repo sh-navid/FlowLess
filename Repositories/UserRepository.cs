2f045ea5926784b2f207060c296e7b2196181fc6
sh-navid <shamshirsaz.navid@live.com>
2025-07-17 09:40:45 +0330

    [Feature] Setup user repository

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,63 @@
+using NoFlowEngine.Models;
+using NoFlowEngine.Data;
+using System.Linq;
+
+namespace NoFlowEngine.Repositories
+{
+    /// <summary>
+    /// UserRepository class implementing the IUserRepository interface.
+    /// Provides data access methods for User entities.
+    /// </summary>
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserRepository"/> class.
+        /// </summary>
+        /// <param name="dbContext">The application's database context.</param>
+        public UserRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        /// <summary>
+        /// Retrieves a user from the database based on the username.
+        /// </summary>
+        /// <param name="username">The username to search for.</param>
+        /// <returns>The User object if found; otherwise, null.</returns>
+        public User? GetUserByUsername(string username)
+        {
+            return _dbContext.Users.FirstOrDefault(u => u.Username == username);
+        }
+
+        /// <summary>
+        /// Adds a new user to the database.
+        /// </summary>
+        /// <param name="user">The User object to add.</param>
+        public void AddUser(User user)
+        {
+            _dbContext.Users.Add(user);
+            _dbContext.SaveChanges();
+        }
+    }
+
+    /// <summary>
+    /// IUserRepository interface defining the contract for User data access.
+    /// </summary>
+    public interface IUserRepository
+    {
+        /// <summary>
+        /// Retrieves a user by username.
+        /// </summary>
+        /// <param name="username">The username to search for.</param>
+        /// <returns>The User object if found; otherwise, null.</returns>
+        User? GetUserByUsername(string username);
+
+        /// <summary>
+        /// Adds a new user.
+        /// </summary>
+        /// <param name="user">The User object to add.</param>
+        void AddUser(User user);
+    }
+}
