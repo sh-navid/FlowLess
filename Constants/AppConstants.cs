dd8599a01bfa650a9073a0ff71b32507865cc0a5
sh-navid <shamshirsaz.navid@live.com>
2025-07-17 08:44:16 +0330

    [Feature] Setup constants

---
+++ Constants/AppConstants.cs
@@ -0,0 +1,13 @@
+namespace NoFlowEngine.Constants
+{
+    /// <summary>
+    /// Contains constant values used throughout the application.
+    /// </summary>
+    public class AppConstants
+    {
+        /// <summary>
+        /// Example key that can be used to store or retrieve example values.
+        /// </summary>
+        public const string ExampleKey = "ExampleKey";
+    }
+}
