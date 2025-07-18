160964ba6d714401e668dc96d88f10b8e6c40fbd
sh-navid <shamshirsaz.navid@live.com>
2025-07-18 08:37:25 +0330

    [Feature] Setup login view model

---
+++ ViewModels/Account/LoginViewModel.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace NoFlowEngine.ViewModels.Account
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string? Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
