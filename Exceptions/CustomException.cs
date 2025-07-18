4e26bd83e7fc37ad4a04296f07085c7499897b44
sh-navid <shamshirsaz.navid@live.com>
2025-07-18 07:09:18 +0330

    [Enhancement] Update documentation for custom exception

--- Exceptions/CustomException.cs
+++ Exceptions/CustomException.cs
@@ -1,12 +1,12 @@
-/// <summary>
-/// A custom exception class that inherits from the base Exception class.
-/// This allows for the creation of specific exception types within the application.
-/// </summary>
-/// <remarks>
-/// This is just an example and can ve removed later
-/// </remarks>
 namespace NoFlowEngine.Exceptions
 {
+    /// <summary>
+    /// A custom exception class that inherits from the base Exception class.
+    /// This allows for the creation of specific exception types within the application.
+    /// </summary>
+    /// <remarks>
+    /// This is just an example and can ve removed later
+    /// </remarks>
     public class CustomException : Exception
     {
         /// <summary>
