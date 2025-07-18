5053ee91c6eafceeacafdb281d29990efdedafc2
sh-navid <shamshirsaz.navid@live.com>
2025-07-18 08:39:41 +0330

    [Enhancement] Include documentation

--- ViewModels/Account/LoginViewModel.cs
+++ ViewModels/Account/LoginViewModel.cs
@@ -2,15 +2,29 @@ using System.ComponentModel.DataAnnotations;
 
 namespace NoFlowEngine.ViewModels.Account
 {
+    /// <summary>
+    /// Represents the view model for user login.
+    /// </summary>
     public class LoginViewModel
     {
-        [Required]
+        /// <summary>
+        /// Gets or sets the username of the user.
+        /// </summary>
+        [Required(ErrorMessage = "Username is required")]
+        [Display(Name = "Username")]
         public string? Username { get; set; }
 
-        [Required]
+        /// <summary>
+        /// Gets or sets the password of the user.
+        /// </summary>
+        [Required(ErrorMessage = "Password is required")]
         [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string? Password { get; set; }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether the user wants to be remembered.
+        /// </summary>
         [Display(Name = "Remember me?")]
         public bool RememberMe { get; set; }
     }
